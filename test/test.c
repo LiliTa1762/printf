c72194a42d623d66434c81e46998ac4e4e88ab66
silenarestrepo <restreposilena@gmail.com>
2020-07-20 20:36:08 +0000

    main test functions

---
+++ test/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_printf("%s", NULL);
+
+
+	return (0);
+}
