9c9ca1054ac6807d9108a40ad083364f651a42a0
silenarestrepo <restreposilena@gmail.com>
2020-07-20 19:54:39 +0000

    header file

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _puts(char *str);
+int _printf(const char *format, ...);
+int match(char format, va_list list);
+
+#endif /* HOLBERTON_H */
